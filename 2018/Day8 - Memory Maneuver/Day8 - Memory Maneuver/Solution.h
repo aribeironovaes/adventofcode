b3256c8495576edaa2c22dd81f4205baec0174f2
Angelo Ribeiro <aribeironovaes@gmail.com>
2018-12-09 09:16:00 -0800

    Fixing Day 8. didn't find the previous bug, just a change on how to reat the stream.

--- 2018/Day8 - Memory Maneuver/Day8 - Memory Maneuver/Solution.h
+++ 2018/Day8 - Memory Maneuver/Day8 - Memory Maneuver/Solution.h
@@ -1,65 +1,52 @@
 #pragma once
 
 #include <list>
+#include <vector>
 
 using namespace std; 
 
 class Solution {
-private: 
-
-	long sumFrontMetadataEntries(list<int>& input, int entriesNumber) {
-		long sum = 0;
-		for (size_t i = 0; i < entriesNumber; i++)
-		{
-			sum += input.front();
-			input.pop_front();
-		}
-		return sum;
-	}
-
-	long sumBackMetadataEntries(list<int>& input, int entriesNumber) {
-		long sum = 0;
-		for (size_t i = 0; i < entriesNumber; i++)
-		{
-			sum += input.back();
-			input.pop_back();
-		}
-		return sum; 
-	}
-
-	void sumMetadata(list<int>& input, int numOfChilds, long& acc) {
-		
-		int currentItemMetadata = input.front();
-		input.pop_front();
-
-		if (numOfChilds == 0) {
-			acc += sumFrontMetadataEntries(input, currentItemMetadata);
-			//Get the number of metadata and sum metadata. 
-		}
-		else {
-			//Get the number of metadata from the end of the list and call with the next item. 
-			acc += sumBackMetadataEntries(input, currentItemMetadata);			
-		}
-
-		if (input.empty()) {
-			return; //Finished.
-		}
-		int numberOfChildsOfFirstItem = input.front();
-		input.pop_front();
-		sumMetadata(input, numberOfChildsOfFirstItem, acc);
-
-	}
 public: 
-	int sumMetadata(list<int>& input) {
-		long returnResult = 0;
-		if (!input.empty()) {
-			int numberOfChildsOfFirstItem = input.front();
-			input.pop_front();
-			//First thing to do is to create the recursive call, passing all the info. 
-			sumMetadata(input, numberOfChildsOfFirstItem, returnResult);
+	void sumMetadata(list<int>& input, int& acc) {
+		int nchildren, nmeta;
+		nchildren = input.front();
+		input.pop_front();
+		nmeta = input.front();
+		input.pop_front();
 
+		for (int i = 0; i < nchildren; i += 1) {
+			sumMetadata(input, acc);
 		}
 
-		return returnResult;
-	};
+		for (int i = 0; i < nmeta; i += 1) {
+			int meta;
+
+			meta = input.front();
+			input.pop_front();
+			acc += meta;
+		}
+	}
+
+
+	int sumMetadata(list<int>& input) {
+		int nchildren, nmeta;
+		int resultReturn = 0;
+		nchildren = input.front();
+		input.pop_front();
+		nmeta = input.front();
+		input.pop_front();
+
+		for (int i = 0; i < nchildren; i += 1) {
+			sumMetadata(input, resultReturn);
+		}
+
+		for (int i = 0; i < nmeta; i += 1) {
+			int meta;
+			meta = input.front();
+			input.pop_front();
+			resultReturn += meta;
+		}
+
+		return resultReturn;
+	}
 };
